fbe48ca74731047870355d9c206d60645ba61ac4
Sadam <msadamramli@gmail.com>
2024-04-04 14:40:09 +0700

    penambahan footer

--- script/chat-write.php
+++ script/chat-write.php
@@ -11,19 +11,3 @@ if (!empty($message)) {
 } else {
   echo "Menerima pesan kosong"; // Respond to the client if no message is received
 }
-?>
-
-<?php
-$message = $_POST['teks'] ?? '';
-$username = $_POST['username'] ?? '';
-
-if (!empty($message)) {
-  $file = fopen("chat.txt", "a"); // Open the file in append mode
-  $data = json_encode(["username" => $username, "message" => $message]); // Encode data to JSON format
-  fwrite($file, $data . "\n"); // Write the data to the file
-  fclose($file); // Close the file
-  echo "Pesan tertulis di file chat.txt"; // Respond to the client
-} else {
-  echo "Menerima pesan kosong"; // Respond to the client if no message is received
-}
-?>
